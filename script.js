436a99e599b4d0029ec3962d235ead5f8fcc2d33
jayaprajapatiITG <jayaprajapati@itgeeks.com>
2025-10-15 20:11:22 +0530

    responsiv process

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+let menuBar = document.querySelector(".menu-bar");
+let navigation = document.querySelector(".navigation");
+menuBar.addEventListener("click", function () {
+
+})
+var swiper = new Swiper(".Swiper-com", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+  },
+});
